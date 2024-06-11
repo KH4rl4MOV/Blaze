76d4cb89646a980bed3b60a12d90e3540221ba08
Anton Makerov <iosmakerov@gmail.com>
2024-06-11 19:45:32 +0200

    processed

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,25 @@
-import 'package:blaze/screen/welcome_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'screen/home_screen.dart';
+import 'screen/welcome_screen.dart';
 import 'theme_provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-void main() {
-  runApp(MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Проверяем, сохранены ли данные пользователя
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  bool userHasData = prefs.containsKey('name') && prefs.getString('name')!.isNotEmpty;
+
+  runApp(MyApp(initialScreen: userHasData ? HomeScreen() : WelcomeScreen()));
 }
 
 class MyApp extends StatelessWidget {
+  final Widget initialScreen;
+
+  MyApp({required this.initialScreen});
+
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
@@ -21,7 +32,7 @@ class MyApp extends StatelessWidget {
               primarySwatch: Colors.blue,
               visualDensity: VisualDensity.adaptivePlatformDensity,
             ),
-            home: WelcomeScreen(),
+            home: initialScreen,
             builder: (context, child) {
               return Container(
                 decoration: BoxDecoration(
