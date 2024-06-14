dd8ea82856602bb4c54149e4cb58605ae62d9616
Anton Makerov <iosmakerov@gmail.com>
2024-06-14 19:05:52 +0200

    processed

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:provider/provider.dart';
 import 'dart:io';
 import 'profile_screen.dart';
 import 'notebook_screen.dart';
 import 'workout_screen.dart';
 import 'settings_screen.dart';
 import 'privacy_policy_screen.dart';
+import '../theme_provider.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -35,99 +37,123 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.black,
-      appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        elevation: 0,
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            CircleAvatar(
-              radius: 50,
-              backgroundImage: _avatarImage != null ? FileImage(_avatarImage!) : AssetImage('assets/empty_avatar.png') as ImageProvider,
-              backgroundColor: Colors.grey,
-            ),
-            SizedBox(height: 16),
-            Text(
-              'Hello, $_userName',
-              style: TextStyle(color: Colors.white, fontSize: 24, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 32),
-            Expanded(
-              child: GridView.count(
-                crossAxisCount: 2,
-                crossAxisSpacing: 16,
-                mainAxisSpacing: 16,
-                children: [
-                  _buildMenuItem(
-                    context,
-                    icon: 'assets/profile_icon.png',
-                    text: 'Profile',
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => ProfileScreen()),
-                      );
-                    },
-                  ),
-                  _buildMenuItem(
-                    context,
-                    icon: 'assets/workout_icon.png',
-                    text: 'Workout',
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => WorkoutScreen()),
-                      );
-                    },
-                  ),
-                  _buildMenuItem(
-                    context,
-                    icon: 'assets/notebook_icon.png',
-                    text: 'Notebook',
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => NotebookScreen()),
-                      );
-                    },
-                  ),
-                  _buildMenuItem(
-                    context,
-                    icon: 'assets/settings_icon.png',
-                    text: 'Settings',
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => SettingsScreen()),
-                      );
-                    },
-                  ),
-                ],
+    return Consumer<ThemeProvider>(
+      builder: (context, themeProvider, child) {
+        return Scaffold(
+          backgroundColor: Colors.black,
+          body: Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage(themeProvider.getBackgroundImage()),
+                fit: BoxFit.cover,
               ),
             ),
-            SizedBox(height: 16),
-            GestureDetector(
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => PrivacyPolicyScreen()),
-                );
-              },
-              child: Padding(
-                padding: const EdgeInsets.only(bottom: 16.0),
-                child: Text(
-                  'Privacy Policy',
-                  style: TextStyle(color: Colors.white, fontSize: 18, fontWeight: FontWeight.bold),
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              children: [
+                Stack(
+                  children: [
+                    Padding(
+                        padding: EdgeInsets.only(top: 64),
+                        child: CircleAvatar(
+                          radius: 50,
+                          backgroundImage: _avatarImage != null
+                              ? FileImage(_avatarImage!)
+                              : AssetImage('assets/empty_avatar.png') as ImageProvider,
+                          backgroundColor: Colors.grey,
+                        ),
+                    ),
+                    Positioned(
+                      bottom: 0,
+                      right: 0,
+                      child: CircleAvatar(
+                        backgroundColor: Colors.white,
+                        radius: 20,
+                        child: Icon(Icons.add_a_photo, color: Colors.black),
+                      ),
+                    ),
+                  ],
                 ),
-              ),
+                SizedBox(height: 16),
+                Text(
+                  'Hello, $_userName',
+                  style: TextStyle(color: Colors.white, fontSize: 24, fontWeight: FontWeight.bold),
+                ),
+                SizedBox(height: 32),
+                Expanded(
+                  child: GridView.count(
+                    crossAxisCount: 2,
+                    crossAxisSpacing: 16,
+                    mainAxisSpacing: 16,
+                    children: [
+                      _buildMenuItem(
+                        context,
+                        icon: 'assets/profile_icon.png',
+                        text: 'Profile',
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) => ProfileScreen()),
+                          );
+                        },
+                      ),
+                      _buildMenuItem(
+                        context,
+                        icon: 'assets/workout_icon.png',
+                        text: 'Workout',
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) => WorkoutScreen()),
+                          );
+                        },
+                      ),
+                      _buildMenuItem(
+                        context,
+                        icon: 'assets/notebook_icon.png',
+                        text: 'Notebook',
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) => NotebookScreen()),
+                          );
+                        },
+                      ),
+                      _buildMenuItem(
+                        context,
+                        icon: 'assets/settings_icon.png',
+                        text: 'Settings',
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) => SettingsScreen()),
+                          );
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(height: 16),
+                GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => PrivacyPolicyScreen()),
+                    );
+                  },
+                  child: Padding(
+                    padding: const EdgeInsets.only(bottom: 16.0),
+                    child: Text(
+                      'Privacy Policy',
+                      style: TextStyle(color: Colors.white, fontSize: 18, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ),
+              ],
             ),
-          ],
-        ),
-      ),
+          ),
+        );
+      },
     );
   }
 
@@ -136,39 +162,35 @@ class _HomeScreenState extends State<HomeScreen> {
       onTap: onTap,
       child: Container(
         decoration: BoxDecoration(
-          color: Color(0xFF0F1A26),
+          color: Color(0xFF0F1A26).withOpacity(0.8),
           borderRadius: BorderRadius.circular(16),
-          //border: Border.all(color: Colors.red, width: 2),
         ),
         child: Padding(
-          padding: const EdgeInsets.all(8.0),
+          padding: const EdgeInsets.all(16.0),  // Устанавливаем отступы
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Row(
-                  children: [
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
-                          text,
-                          style: TextStyle(color: Colors.white, fontSize: 18, fontWeight: FontWeight.bold),
-                        ),
-                        SizedBox(height: 40),
-                        Image.asset(icon, height: 40, color: Colors.red),
-                      ],
-                    ),
-                    Spacer(),
-                    Container(
-                      height: 120,
-                      width: 2,
-                      color: Colors.red,
-                    ),
-                  ],
-                ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,  // Разделяем текст и иконку
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        text,
+                        style: TextStyle(color: Colors.white, fontSize: 18, fontWeight: FontWeight.bold),
+                      ),
+                      SizedBox(height: 32),
+                      Image.asset(icon, height: 40, color: Colors.red),
+                    ],
+                  ),
+                  Container(
+                    height: 120,  // Высота полоски равна высоте кнопки с отступами
+                    width: 2,
+                    color: Colors.red,
+                  ),
+                ],
               ),
             ],
           ),
